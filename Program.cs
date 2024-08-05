b2b48b4fa5bde5ca36dee22e813c64135f1d7e50
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-05 17:10:30 -0600

    Cambio 6 "Detalles"

--- Program.cs
+++ Program.cs
@@ -1,47 +1,70 @@
 ﻿using Actividad_Herencia;
 Telefono telefono = new Telefono(0,null,0,0);
 bool menuCondition = true;
-do
+try
 {
-    Console.Clear();
-    Console.WriteLine("___MENU___");
-    Console.WriteLine("1. Agregar nuevos productos.");
-    Console.WriteLine("2. Mostrar todos los productos registrados.");
-    Console.WriteLine("3. Buscar producto por nombre.");
-    Console.Write("Escriba una opción: ");
-    try
+    do
     {
-        int option = int.Parse(Console.ReadLine());
-        switch (option)
+        Console.Clear();
+        Console.WriteLine("___MENU___");
+        Console.WriteLine("1. Agregar nuevos productos.");
+        Console.WriteLine("2. Mostrar todos los productos registrados.");
+        Console.WriteLine("3. Buscar producto por nombre.");
+        Console.WriteLine("4. Salir");
+        Console.Write("Escriba una opción: ");
+        try
         {
-            case 1:
-                Console.Clear();
-                Console.Write("Ingrese el tipo de producto que quiere ingresar (1.Teléfono, 2.Ordenador, 3.Tablet): ");
-                int tipoProducto = int.Parse(Console.ReadLine());
-                if (tipoProducto == 1)
-                {
-                    telefono.RegistrarTelefonos();
-                }
-                Console.ReadKey();
-                break;
-            case 2:
-                Console.Clear();
-                telefono.MostrarTelefonos();
-                Console.ReadKey();
-                break;
-            case 3:
-                Console.Clear();
-                Console.ReadKey();
-                break;
-            case 4:
-                menuCondition = false;
-                break;
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.Write("Ingrese el tipo de producto que quiere ingresar (1.Teléfono, 2.Ordenador, 3.Tablet): ");
+                    int tipoProducto = int.Parse(Console.ReadLine());
+                    if (tipoProducto == 1)
+                    {
+                        telefono.RegistrarTelefonos();
+                    }
+                    else if (tipoProducto == 2)
+                    {
+
+                    }
+                    else if (tipoProducto == 3)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("No es una opción válida");
+                        Console.ReadKey();
+                    }
+                    Console.ReadKey();
+                    break;
+                case 2:
+                    Console.Clear();
+                    telefono.MostrarTelefonos();
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    menuCondition = false;
+                    break;
+            }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
     }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-    }
+    while (menuCondition);
 
 }
-while (menuCondition);
+finally
+{
+    Console.WriteLine("\nGracias por usar nuestro sistema...");
+}
--- Télefono.cs
+++ Télefono.cs
@@ -9,7 +9,6 @@ namespace Actividad_Herencia
     public class Telefono : Producto
     {
         public List<Telefono> listTelefonos = new List<Telefono>();
-        int id = 5020;
         public int Id { get; set; }
         public Telefono(int id, string nombre, double precio, int stock)
         {
@@ -20,6 +19,7 @@ namespace Actividad_Herencia
         }
         public void RegistrarTelefonos()
         {
+            int id = 5020;
             Console.WriteLine("Ingrese la cantidad de productos que quiere registrar: ");
             int cantidad = int.Parse(Console.ReadLine());
             for (int i = 0; i < cantidad; i++)
@@ -27,23 +27,28 @@ namespace Actividad_Herencia
                 id += 1;
                 Console.Write("Ingrese el nombre del producto: ");
                 string nombre = Console.ReadLine();
-                Console.Write("Ingrese el precio del producto: ");
+                Console.Write("Ingrese el precio del producto: Q");
                 double precio = double.Parse(Console.ReadLine());
                 Console.Write("Ingrese el stock del producto: ");
                 int stock = int.Parse(Console.ReadLine());
                 listTelefonos.Add(new Telefono(id, nombre, precio, stock));
             }
+            Console.Clear();
+            Console.WriteLine("Productos registrados correctamente.");
+            Console.WriteLine("\nPresione cualquier tecla para continuar: ");
         }
         public void MostrarTelefonos()
         {
             Console.WriteLine("Información de los teléfonos registrados: ");
             foreach (Telefono telefono in listTelefonos)
             {
-                Console.WriteLine($"El ID del producto: {telefono.id}");
+                Console.WriteLine($"\nEl ID del producto: {telefono.Id}");
                 Console.WriteLine($"El nombre del producto: {telefono.Name}");
-                Console.WriteLine($"El precio del producto: {telefono.Price}");
-                Console.WriteLine($"El stock del producto: {telefono.Stock}");
+                Console.WriteLine($"El precio del producto: Q{telefono.Price}");
+                Console.WriteLine($"El stock del producto: {telefono.Stock} unidades");
             }
+            Console.WriteLine("Presione cualquier tecla para continuar: ");
+            Console.ReadKey();
         }
     }
 }
