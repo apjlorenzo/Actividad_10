2ab727c609217862306de51cc42806ec03a4b4fe
Pablo Lorenzo <apjlorenzo1@gmail.com>
2024-08-05 13:18:45 -0600

    Cambio 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool menuCondition = true;
+do
+{
+    Console.WriteLine("___MENU___");
+    Console.WriteLine("1. Agregar nuevos productos.");
+    Console.WriteLine("2. Mostrar todos los productos registrados.");
+    Console.WriteLine("3. Buscar producto por nombre.");
+    Console.Write("Escriba una opción: ");
+    int option = int.Parse(Console.ReadLine());
+    
+    switch(option)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+    }
+
+}
+while (menuCondition);
